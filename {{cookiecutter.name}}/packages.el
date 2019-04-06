4b61028bba191c7d7a8fcd1df7ed46288c2674f0
Médéric Hurier <fmind@fmind.me>
2019-04-06 14:33:35 +0200

    initial commit

---
+++ {{cookiecutter.name}}/packages.el
@@ -0,0 +1,8 @@
+;; package configurations
+;; use use-package to speed-up the loading
+;; for each package, you may define the following functions:
+;; - <layer>/init-<package>
+;; - <layer>/pre-init-<package>
+;; - <layer>/post-init-<package>
+
+(defconst {{cookiecutter.name}}-packages '())
